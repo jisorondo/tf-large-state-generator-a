9b90043648999ee5af73154f0ce524e973d90d32
Jorge Sorondo <87992063+jisorondo@users.noreply.github.com>
2024-01-24 16:02:44 -0600

    Create variables.tf

---
+++ examples/variables.tf
@@ -0,0 +1,11 @@
+variable "pet_words" {
+  default     = 10
+  description = "Number of words per pet"
+}
+variable "pets_count" {
+  default     = 750
+  description = "Count of pets"
+}
+variable "string_length" {
+  default = 512
+}
